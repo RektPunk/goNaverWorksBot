76fa54742cb431ba1fcc265b690c6531c0dfc969
unknown <rektpunk@gmail.com>
2025-12-05 11:20:12 +0900

    [Feature] add current timestamp in system prompt

--- internal/server/openai_helper.go
+++ internal/server/openai_helper.go
@@ -1,19 +1,24 @@
 package server
 
 import (
+	"fmt"
 	"goNaverWorksBot/internal/db"
+	"time"
 
 	"github.com/openai/openai-go/v3"
 )
 
-const systemPrompt string = `You are RektPunkBot, an insightful, encouraging assistant. 
+const baseSystemPrompt string = `You are RektPunkBot, an insightful, encouraging assistant. 
 Do not explain your role. Answer the user's questions directly, concisely, and exclusively. 
-Refer to the previous conversation history to continue the dialogue naturally.`
+Refer to the previous conversation history to continue the dialogue naturally.
+[Knowledge cutoff]: 2024-06`
 const maxTokens int64 = 500
 
 func (h *Handler) convertHistoryToMessagesParams(history []db.ChatTurn, userTurn db.ChatTurn) openai.ChatCompletionNewParams {
+	today := time.Now()
+	dynamicSystemPrompt := fmt.Sprintf("%s\n[Current Timestamp]: %s", baseSystemPrompt, today)
 	messages := []openai.ChatCompletionMessageParamUnion{
-		openai.SystemMessage(systemPrompt),
+		openai.SystemMessage(dynamicSystemPrompt),
 	}
 	for _, turn := range history {
 		switch turn.Role {
